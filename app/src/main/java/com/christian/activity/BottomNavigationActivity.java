62a9bf88557b7910c24e82ac81b27db8ab88905e
Administrator <lanhuaguizha@outlook.com>
2017-04-02 17:43:06 +0800

    HomeFragment, BookFragment, MusicFragment added

--- app/src/main/java/com/christian/activity/BottomNavigationActivity.java
+++ app/src/main/java/com/christian/activity/BottomNavigationActivity.java
@@ -1,4 +1,4 @@
-package com.christian;
+package com.christian.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.christian.R;
+
+public class BottomNavigationActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -18,13 +20,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.large_text);
                     return true;
                 case R.id.navigation_book:
                     mTextMessage.setText(R.string.title_book);
                     return true;
                 case R.id.navigation_music:
                     mTextMessage.setText(R.string.title_music);
+                    return true;
+                case R.id.navigation_video:
+                    mTextMessage.setText(R.string.title_video);
                     return true;
             }
             return false;
@@ -35,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bottom_navigation);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
---
+++ app/src/main/java/com/christian/fragment/BookFragment.java
@@ -0,0 +1,10 @@
+package com.christian.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/4/2.
+ */
+
+public class BookFragment extends Fragment {
+}
