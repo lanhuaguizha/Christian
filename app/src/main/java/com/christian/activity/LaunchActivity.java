ac270174ec6377a76582534fc26647faedd4f712
Administrator <lanhuaguizha@outlook.com>
2017-04-12 21:45:40 +0800

    Adding launch screen

--- app/src/main/java/com/christian/activity/LaunchActivity.java
+++ app/src/main/java/com/christian/activity/LaunchActivity.java
@@ -1,10 +1,24 @@
 package com.christian.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+import com.christian.R;
+
+import org.xutils.view.annotation.ContentView;
+
 /**
  * author：Administrator on 2017/4/12 00:17
  * email：lanhuaguizha@gmail.com
  * Launch screen activity for the first be started activity.
  */
 
+@ContentView(R.layout.activity_launch)
 public class LaunchActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
