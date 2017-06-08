285b0185425c0208bcfd8bda26ac723dc453bd77
Administrator <lanhuaguizha@outlook.com>
2017-06-08 15:35:07 +0800

    set GONE to let the FAB not to click

--- app/src/main/java/com/christian/activity/HomeDetailActivity.java
+++ app/src/main/java/com/christian/activity/HomeDetailActivity.java
@@ -79,6 +79,12 @@ public class HomeDetailActivity extends BaseActivity {
                     } else {
                         floatingActionButton.animate().scaleX(0).scaleY(0);
                     }
+                    // set GONE to let the FAB not to click
+                    if (v.getChildAt(0).getHeight() - scrollY - v.getHeight() > 400 && scrollY > 400) {
+                        floatingActionButton.setVisibility(View.GONE);
+                    } else {
+                        floatingActionButton.setVisibility(View.VISIBLE);
+                    }
                 }
             });
     }
