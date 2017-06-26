699c6e1e622ea46165c99ecdbcd524ab59b56ae4
Administrator <lanhuaguizha@outlook.com>
2017-06-26 08:01:41 +0800

    Home detail first in not show toolbar

--- app/src/main/java/com/christian/activity/GospelHomeDetailActivity.java
+++ app/src/main/java/com/christian/activity/GospelHomeDetailActivity.java
@@ -176,6 +176,7 @@ public class GospelHomeDetailActivity extends BaseActivity {
     }
 
     private void loadGospelDetail() {
+        appBarLayout.setExpanded(false, false);
         gospelDetail.postDelayed(new Runnable() {
             @Override
             public void run() {
