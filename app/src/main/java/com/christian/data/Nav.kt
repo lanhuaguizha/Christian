f7cad878818f1be633dc54d0dda91a93595136d7
lanhuaguizha <lanhuaguizha@outlook.com>
2018-04-05 12:45:56 +0800

    nav_item_view, max lines set 2

--- app/src/main/java/com/christian/data/Nav.kt
+++ app/src/main/java/com/christian/data/Nav.kt
@@ -11,11 +11,11 @@ import java.util.*
  */
 @Entity(tableName = "navs")
 data class Nav @JvmOverloads constructor(
-        @ColumnInfo(name = "subtitle") var subtitle: String = "John",
-        @ColumnInfo(name = "title") var title: String = "3:16",
-        @ColumnInfo(name = "detail") var detail: String = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.",
+        @ColumnInfo(name = "subtitle") var subtitle: String = "约翰福音",
+        @ColumnInfo(name = "title") var title: String = "叫一切信他的，不至灭亡，反得永生。",
+        @ColumnInfo(name = "detail") var detail: String = "神爱世人，甚至将他的独生子赐给他们，叫一切信他的，不至灭亡，反得永生。",
         @ColumnInfo(name = "relation") var relation: String = "",
-        @ColumnInfo(name = "author") var author: String = "Holy Spirit",
+        @ColumnInfo(name = "author") var author: String = "神",
         @PrimaryKey @ColumnInfo(name = "id") var id: String = UUID.randomUUID().toString()
 ) {
 
