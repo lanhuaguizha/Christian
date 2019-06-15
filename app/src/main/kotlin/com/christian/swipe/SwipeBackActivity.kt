d40e8b6dcc88a29424d93378bd6d843f9e7011e0
lanhuaguizha <lanhuaguizha@outlook.com>
2019-06-15 23:26:51 +0800

    Swipe back same as Flutter design

--- app/src/main/kotlin/com/christian/swipe/SwipeBackActivity.kt
+++ app/src/main/kotlin/com/christian/swipe/SwipeBackActivity.kt
@@ -29,8 +29,8 @@ abstract class SwipeBackActivity : AppCompatActivity() {
         val parallaxBackLayout = ParallaxHelper.getParallaxBackLayout(this, true)
         parallaxBackLayout.setLayoutType(ParallaxBackLayout.LAYOUT_CUSTOM, CupertinoParallaxTransform())
         parallaxBackLayout.setShadowDrawable(AppCompatResources.getDrawable(this@SwipeBackActivity, R.drawable.bga_sbl_shadow))
-        parallaxBackLayout.setScrollThresHold(0.2f)
-        parallaxBackLayout.setVelocity(Int.MAX_VALUE)
+        parallaxBackLayout.setScrollThresHold(0.5f)
+        parallaxBackLayout.setVelocity(1500)
     }
 
     override fun onBackPressed() {
