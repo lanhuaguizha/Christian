3e99d2cfc71809889ffc3c382b3648ba9be62f23
paultao <paultao@pptv.com>
2019-06-21 09:40:23 +0800

    Page slides 1/5 and closes

--- app/src/main/kotlin/com/christian/swipe/SwipeBackActivity.kt
+++ app/src/main/kotlin/com/christian/swipe/SwipeBackActivity.kt
@@ -29,7 +29,7 @@ abstract class SwipeBackActivity : AppCompatActivity() {
         val parallaxBackLayout = ParallaxHelper.getParallaxBackLayout(this, true)
         parallaxBackLayout.setLayoutType(ParallaxBackLayout.LAYOUT_CUSTOM, CupertinoParallaxTransform())
         parallaxBackLayout.setShadowDrawable(AppCompatResources.getDrawable(this@SwipeBackActivity, R.drawable.bga_sbl_shadow))
-        parallaxBackLayout.setScrollThresHold(1/3f)
+        parallaxBackLayout.setScrollThresHold(0.2f)
     }
 
     override fun onBackPressed() {
