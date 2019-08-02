a915d0c89ff3b33190c178cbc8415819e4b016ff
paultao <paultao@pptv.com>
2019-08-02 13:21:50 +0800

    jsr305

---
+++ library/src/main/java/com/christian/library/annotation/Test.kt
@@ -0,0 +1,6 @@
+package com.christian.library.annotation
+
+fun main() {
+    val session = Session()
+    session.setD(null)
+}
---
+++ library/src/main/java/com/christian/library/annotation/TestJava.java
@@ -0,0 +1,9 @@
+package com.christian.library.annotation;
+
+public class TestJava {
+
+    public static void main(String[] args) {
+        Session session = new Session();
+        session.setD(null);
+    }
+}
