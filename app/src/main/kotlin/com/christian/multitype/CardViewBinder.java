26aab17b5439063204577664897e92b1d0dd1abc
yongqiangtao <lanhuaguizha@outlook.com>
2019-12-30 17:19:49 +0800

    text image to show up

--- app/src/main/kotlin/com/christian/multitype/CardViewBinder.java
+++ app/src/main/kotlin/com/christian/multitype/CardViewBinder.java
@@ -1,19 +1,47 @@
 package com.christian.multitype;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.target.Target;
 import com.christian.R;
 
 import com.christian.multitype.Card;
+
+import org.commonmark.node.FencedCodeBlock;
+import org.commonmark.node.Paragraph;
+
+import java.util.Collection;
+import java.util.Collections;
+
 import me.drakeet.multitype.ItemViewBinder;
+import ru.noties.markwon.AbstractMarkwonPlugin;
 import ru.noties.markwon.Markwon;
+import ru.noties.markwon.MarkwonConfiguration;
+import ru.noties.markwon.MarkwonSpansFactory;
+import ru.noties.markwon.MarkwonVisitor;
+import ru.noties.markwon.core.CorePlugin;
+import ru.noties.markwon.core.MarkwonTheme;
+import ru.noties.markwon.image.AsyncDrawable;
+import ru.noties.markwon.image.AsyncDrawableLoader;
+import ru.noties.markwon.image.ImageItem;
+import ru.noties.markwon.image.ImagesPlugin;
+import ru.noties.markwon.image.SchemeHandler;
+import ru.noties.markwon.image.network.NetworkSchemeHandler;
 
 /**
  * @author drakeet
@@ -21,12 +49,12 @@ import ru.noties.markwon.Markwon;
 @SuppressWarnings("WeakerAccess")
 public class CardViewBinder extends ItemViewBinder<Card, CardViewBinder.ViewHolder> {
 
-    private Context mContext;
+    private Context context;
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {
-        mContext = parent.getContext();
+        context = parent.getContext();
         View root = inflater.inflate(R.layout.about_page_item_card, parent, false);
         return new ViewHolder(root);
     }
@@ -34,10 +62,51 @@ public class CardViewBinder extends ItemViewBinder<Card, CardViewBinder.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, @NonNull Card card) {
         // obtain an instance of Markwon
-        final Markwon markwon = Markwon.create(mContext);
+//        final String markdown = "`A code` that is rendered differently\n\n```\nHello!\n```";
+
+//        final Markwon markwon = Markwon.builder(context)
+//                .usePlugin(new AbstractMarkwonPlugin() {
+//                    @Override
+//                    public void configureTheme(@NonNull MarkwonTheme.Builder builder) {
+//                        builder
+//                                .codeBackgroundColor(Color.BLACK)
+//                                .codeTextColor(Color.RED);
+//                    }
+//                })
+//                .build();
+        final String markdown = "![image](myownscheme://en.wikipedia.org/static/images/project-logos/enwiki-2x.png)";
+        final Markwon markwon = Markwon.builder(context)
+                .usePlugin(CorePlugin.create())
+                .usePlugin(new AbstractMarkwonPlugin() {
+                    @Override
+                    public void configureSpansFactory(@NonNull MarkwonSpansFactory.Builder builder) {
+                        builder.setFactory(Paragraph.class, (configuration, props) ->
+                                new ForegroundColorSpan(Color.GREEN));
+                    }
+                })
+                .usePlugin(ImagesPlugin.create(context))
+                .usePlugin(new AbstractMarkwonPlugin() {
+                    @Override
+                    public void configureImages(@NonNull AsyncDrawableLoader.Builder builder) {
+                        // we can have a custom SchemeHandler
+                        // here we will just use networkSchemeHandler to redirect call
+                        builder.addSchemeHandler("myownscheme", new SchemeHandler() {
+
+                            final NetworkSchemeHandler networkSchemeHandler = NetworkSchemeHandler.create();
+
+                            @Nullable
+                            @Override
+                            public ImageItem handle(@NonNull String raw, @NonNull Uri uri) {
+                                raw = raw.replace("myownscheme", "https");
+                                return networkSchemeHandler.handle(raw, Uri.parse(raw));
+                            }
+                        });
+                    }
+                })
+                .build();
         // set markdown
-        markwon.setMarkdown(holder.content, card.content.toString().replace("\\n", "\n"));
-//    holder.content.setText(card.content);
+        markwon.setMarkdown(holder.content, markdown);
+//        markwon.setMarkdown(holder.content, card.content.toString());
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
