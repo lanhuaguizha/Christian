92225b44d858c98813e50dd13d6f567f309ed612
taoyongqiang <1105832354@qq.com>
2020-11-27 17:01:01 +0800

    未完成popup menu圆角化

--- xpopup/src/main/java/com/lxj/xpopup/util/ViewUtil.kt
+++ xpopup/src/main/java/com/lxj/xpopup/util/ViewUtil.kt
@@ -2,10 +2,11 @@ package com.lxj.xpopup.util
 
 import android.view.ViewGroup
 import android.view.Window
+import androidx.cardview.widget.CardView
 import com.eightbitlab.supportrenderscriptblur.SupportRenderScriptBlur
 import eightbitlab.com.blurview.BlurViewExtendsVerticalRecyclerView
 
-fun makeViewBlur(viewVertical: BlurViewExtendsVerticalRecyclerView, parent: ViewGroup, window: Window, boolean: Boolean = false) {
+fun makeViewBlur(viewVertical: CardView, parent: ViewGroup, window: Window, boolean: Boolean = false) {
     val windowBackground = window.decorView.background
     val radius = 25f
     viewVertical.setupWith(parent)
